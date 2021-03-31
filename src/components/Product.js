9d507cdb8743fafbac18d7d35a8e25073beb4ba3
cıkay <muzaffer123cikay@gmail.com>
2021-03-31 14:39:27 +0300

    aligned rating star icon

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,31 +17,29 @@ export default function Product({ productInfo }) {
   }
 
   return (
-    
-      <Card style={{ height: '400px' }}>
-        <Card.Body style={{ height: '100%', width: '100%' }}>
-          <img className='rounded mx-auto d-block' src={productInfo.img} />
-          <p className='text-center' style={{ marginTop: '10px' }}>
-            <IoMdStarOutline display='inline' />
-            <span>
-              {productInfo.rating}{' '}
-              <span className='text-muted text-capitalize'>
-                ({productInfo.comment} Yorum)
-              </span>{' '}
-            </span>
-          </p>
-          <p className='text-center'>{productInfo.title}</p>
-          <div className='text-primary text-center mb-auto p-2'>
-            <span className='font-weight-bold'>
-              {productInfo.cur && getCurrencyIcon(productInfo.cur)}
+    <Card style={{ height: '400px' }}>
+      <Card.Body style={{ height: '100%', width: '100%' }}>
+        <img className='rounded mx-auto d-block' src={productInfo.img} />
+        <p className='text-center' style={{ marginTop: '10px' }}>
+          <IoMdStarOutline display='inline' style={{ marginBottom: '4px' }} />
+          <span>
+            {productInfo.rating}{' '}
+            <span className='text-muted text-capitalize'>
+              ({productInfo.comment} Yorum)
             </span>{' '}
-            {productInfo.price}
-          </div>
-        </Card.Body>
-        <Button variant='primary' onClick={handleClick}>
-          Sepete Ekle
-        </Button>
-      </Card>
-    
+          </span>
+        </p>
+        <p className='text-center'>{productInfo.title}</p>
+        <div className='text-primary text-center mb-auto p-2'>
+          <span className='font-weight-bold'>
+            {productInfo.cur && getCurrencyIcon(productInfo.cur)}
+          </span>{' '}
+          {productInfo.price}
+        </div>
+      </Card.Body>
+      <Button variant='primary' onClick={handleClick}>
+        Sepete Eklepx
+      </Button>
+    </Card>
   )
 }
